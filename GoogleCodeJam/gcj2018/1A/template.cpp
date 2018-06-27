5b30f4ba9dee0069b1ffad5c08e993b4543c7570
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2018-06-27 18:59:12 -0400

    Add ./GoogleCodeJam/gcj2018/1A/template.cpp

---
+++ GoogleCodeJam/gcj2018/1A/template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define _USE_MATH_DEFINES
+#include <cmath> 
+
+using namespace std;
+
+
+
+typedef long long ll;
+
+int main() {
+
+  cout << "hello world" <<endl;
+}
+
