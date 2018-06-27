26e8a34dbe8826bbb08c4c06d10cd448363320f6
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2018-06-27 18:59:24 -0400

    Add ./Kattis/free/2017/Jun/bikingduck.cpp

---
+++ Kattis/free/2017/Jun/bikingduck.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int vw, vb;
+int x1, y1, x2, y2;
+int xg, yg;
+int xd, yd;
+int n;:
+int Xs[1000];
+int Ys[1000];
+
+double 
+
+int main() {
+    cin >> vw >> vb;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> xg >> yg;
+    cin >> xd >> yd;
+    cin >> n;
+
+    for(int i=0; i<n; i++) {
+        cin >> Xs[i] >> Ys[i];
+    }
+
+    for(int i=
+
+
+
+
+
+
+
