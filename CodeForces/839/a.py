3b7f2114ad1e7eb1879a31df98bf3827a0865ff9
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2018-06-27 18:59:23 -0400

    Add ./CodeForces/839/a.py

---
+++ CodeForces/839/a.py
@@ -0,0 +1,15 @@
+n,k = map(int, input().split())
+A = list(map(int, input().split()))
+
+arya = 0
+bran = 0
+
+for i,a in enumerate(A):
+    arya += a 
+    bran += min(arya, 8)
+    arya -= min(arya, 8)
+    if bran >= k:
+        print(i+1)
+        exit()
+
+print(-1)
