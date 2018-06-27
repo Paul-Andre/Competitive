35c473a00fc6ea28bd78959089d1e5a5f9949409
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2018-06-27 18:58:57 -0400

    Add ./HackerRank/26May2017/trip.cpp

---
+++ HackerRank/26May2017/trip.cpp
@@ -0,0 +1,546 @@
+//Mo's algorithm y'all
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+typedef pair<ll,ll> pll;
+
+ll W[200000];
+ll G[200000];
+ll P[200000];
+
+class Seg {
+    public:
+    int a;
+    int b;
+    ll req;
+    ll ret;
+
+    Seg *left;
+    Seg *right;
+
+    Seg() {}
+
+    Seg(int a_, int b_) {
+        a = a_;
+        b = b_;
+        if ( a == b) {
+            if (G[a] >= W[a]) {
+                req = 0;
+                ret = G[a]-W[a];
+            }
+            else {
+                req = W[a] - G[a];
+                ret = 0;
+            }
+        }
+        else {
+            int c = (a+b)/2;
+
+            left = new Seg(a,c);
+            right = new Seg(c+1,b);
+
+            if (right->req >= left->ret) {
+                req = left->req + right->req - left->ret;
+                ret = right->ret;
+            }
+            else {
+                req = left->req;
+                ret = right->ret + left->ret - right->req;
+            }
+        }
+    }
+
+    pll query(int aa, int bb) {
+        if (bb< a || aa> b) {
+            return pll(0,0);
+        }
+        ////cerr << "aa bb " << aa << " " << bb << endl;
+        ////cerr << "a b " << a << " " <<b << endl;
+        if (aa<= a && bb>=b) {
+
+            //cerr << "wew\n";
+            return pll(req, ret);
+        }
+        
+        pll l = left->query(aa, bb);
+        pll r = right->query(aa, bb);
+
+        ll q, t;
+
+        if (r.first >= l.second) {
+            q = l.first + r.first - l.second;
+            t = r.second;
+        }
+        else {
+            q = l.first;
+            t = r.second + l.second - r.first;
+        }
+
+        return pll(q,t);
+    }
+
+};
+
+Seg seg;
+
+
+bool check(int a, int b, ll payy, ll gass, int topp, deque<int> &dq) {
+    return true;
+    ll payed = 0;
+    ll gas = 0;
+    vector<int> minns;
+    int prev_best = P[a];
+    int prev_best_i = a;
+    //minns.push_back(a);
+
+    for (int i=a+1; i<=b; i++) {
+        if (P[i] < prev_best || i==b) {
+
+            bool used = false;
+            for (int j=prev_best_i; j<i; j++) {
+                gas += G[j];
+                if (gas < W[j]) {
+                    ll needGas = W[j]-gas;
+                    gas += needGas;
+                    payed += prev_best*needGas;
+                    used = true;
+                }
+                gas -= W[j];
+            }
+            if (used) {
+
+                    minns.push_back(prev_best_i);
+            }
+
+            prev_best = P[i];
+            prev_best_i = i;
+
+
+        }
+    }
+    cerr << "a b: " << a << " " << (b-1) << endl;
+    cerr << "Payed: " << payy << endl;
+    cerr << "Gas: " << gass << endl;
+    cerr << "ToppestLow: " << topp << endl;
+    cerr << "Deque: ";
+    for (int i=0; i<dq.size(); i++) {
+        cerr << dq[i] << " ";
+    }
+
+    bool same = true;
+    for (int i=0; i<dq.size() && i<minns.size(); i++) {
+        same = same && dq[i] == minns[i];
+    }
+    cerr << endl;
+
+    bool ret = same && payed == payy && gas == gass && dq.size() == minns.size()
+        && topp>=a && topp<b;
+
+    if (!ret) {
+        cerr << "Brute Payed: " << payed << endl;
+        cerr << "Brute Gas: " << gas << endl;
+        cerr << "Brute Deque: ";
+        for (int i=0; i<minns.size(); i++) {
+            cerr << minns[i] << " ";
+        }
+        cerr << endl;
+    }
+
+    return ret;
+}
+
+
+pll Q[200000];
+int reord[200000];
+ll ans[200000];
+
+int n, q;
+
+int sqrtN;
+
+
+bool ssort(int a, int b) {
+    //true if A comes before B
+    //
+    pll A = Q[a];
+    pll B = Q[b];
+
+    int as = (A.second-1) / sqrtN;
+    int bs = (B.second-1) / sqrtN;
+
+    if (as!= bs) {
+        return as > bs;
+    }
+    return A.first > B.first;
+}
+
+
+int main() {
+    cin >> n >> q;
+
+    sqrtN = (int)(sqrt(n));
+
+    for (int i=0; i<n-1; i++) {
+        cin >> W[i];
+    }
+    for (int i=0; i<n; i++) {
+        cin >> G[i] >> P[i];
+    }
+
+    seg = Seg(0, n-2);
+
+    for (int i=0; i<q; i++) {
+        ll a;
+        ll b;
+        cin >> a >> b;
+        a--;
+        b--;
+        /*
+        pll q = seg.query(a,b);
+        cerr << q.first << " " << q.second << endl;
+        */
+        Q[i] = pll(a,b);
+        reord[i] = i;
+    }
+
+
+    sort(reord, reord+q, ssort);
+
+    int prevSec = -1000;
+
+    deque<int> dq;
+
+    int a, b;
+    ll payed;
+    ll gas;
+    int toppestLow;
+    bool toppestLowInQueue;
+
+    for (int ii = 0; ii<q; ii++) {
+
+        pll query = Q[reord[ii]];
+        ll aa = query.first;
+        ll bb = query.second;
+
+        if (aa == bb) {
+            ans[reord[ii]] = 0;
+            continue;
+        }
+
+        bb-=1;
+
+        //cerr << "aa bb " << aa << " " << bb << endl;
+
+        int sec = bb / sqrtN;
+
+        if ( sec != prevSec) {
+            //cerr << "new secc\n";
+            a = b = min(n-2, sec * sqrtN + sqrtN - 1);
+            dq = deque<int>();
+
+            pll q  = seg.query(a,a);
+            payed = q.first*P[a];
+            gas = q.second;
+
+            if ( q.first != 0) {
+                dq.push_back(a);
+                toppestLowInQueue = true;
+            }
+            else toppestLowInQueue = false;
+
+            toppestLow = a;
+            prevSec = sec;
+            assert(check(a,b, payed, gas,toppestLow, dq));
+        }
+
+        while(a>aa) {
+            ////cerr << "a <<\n";
+            a--;
+
+            int end = b;
+            if (!dq.empty()) {
+
+                if ( dq.front() != a+1) {
+                    dq.push_front(a+1);
+                }
+                
+
+                ll hasPayed = 0;
+                ll hadGas = 0;
+                int prev_best = P[a+1];
+                int prev_best_i = a+1;
+
+
+                while(prev_best >= P[a] && prev_best_i != b+1) {
+                    ////cerr << "Removing the unneeded\n";
+                    //cerr << "dq size " << dq.size() << endl;
+                    if (dq.front() == toppestLow) {
+                        toppestLowInQueue = false;
+                    }
+                    dq.pop_front();
+
+                    int i;
+
+                    if (!dq.empty()) {
+                        i = dq.front();
+                    }
+                    else if (dq.empty()) {
+                        i = b+1;
+                    }
+
+                    pll q = seg.query(prev_best_i, i-1);
+
+                    if (hadGas < q.first) {
+                        ll needGas = q.first - hadGas;
+                        hadGas += needGas;
+                        hasPayed += prev_best*needGas;
+                    }
+
+                    hadGas -= q.first;
+                    hadGas += q.second;
+
+                    prev_best = P[i];
+                    prev_best_i = i;
+                }
+
+
+                //cerr << "substracting " << hasPayed << endl;
+                payed -= hasPayed;
+
+                while(true) {
+
+                    if (!dq.empty() && dq.front() != a+1) {
+                        //cerr << "Making sure removing what was reused\n";
+                        int i = dq.front();
+                        int j;
+                        if (dq.size()>1) {
+                            dq.pop_front();
+                            j = dq.front();
+                            dq.push_front(i);
+                        }
+                        else {
+                            j = b+1;
+                        }
+
+                        pll q = seg.query(i, j-1);
+                        //cerr << "hadGas = " << hadGas << endl;
+                        assert(q.first > hadGas);
+
+                        payed += hadGas*P[i];
+                    }
+
+
+                    pll q = seg.query(a, prev_best_i - 1);
+
+                    if ( dq.empty() ) {
+                        //cerr << "dq.empty add " << q.first*P[a] << endl;
+                        payed += q.first*P[a];
+                        end = prev_best_i - 1;
+                        gas = q.second;
+                        break;
+                    }
+
+                    else {
+                        int i = dq.front();
+                        assert(i==prev_best_i);
+                        int j;
+                        if (dq.size()> 1) {
+                            dq.pop_front();
+                            j = dq.front();
+                            dq.push_front(i);
+                        }
+                        else {
+                            j = b+1;
+                        }
+
+                        pll qq = seg.query(i, j-1);
+
+
+                        //cerr << "i j " << i << " " << j << endl;
+                        //cerr << "q " << q.first << " " << q.second << endl;
+                        //cerr << "qq " << qq.first << " " << qq.second << endl;
+
+                        if (qq.first <= q.second) {
+                            if (toppestLow == dq.front() ) {
+                                toppestLowInQueue = false;
+                            }
+                            dq.pop_front();
+                            prev_best_i = j;
+                            //cerr << "Removing what would be freeby\n";
+                            payed -= qq.first*P[i];
+                            hadGas = qq.second;
+
+                        }
+                        else {
+                            //cerr << "dq.not empty sub " << -(q.second-qq.first)*P[i]<< endl;
+                            //cerr << "dq.not empy add " << q.first*P[a] << endl;
+
+                            payed -= (q.second) * P[i];
+                            payed += q.first*P[a];
+                            end = prev_best_i - 1;
+                            break;
+                        }
+                    }
+                }
+            }
+            else {  // dq empty
+                pll q = seg.query(a, b);
+                payed = q.first*P[a];
+
+                gas = q.second;
+            }
+
+
+
+            pll q = seg.query(a, end);
+
+            if (P[toppestLow] >= P[a]) {
+                toppestLow = a;
+                toppestLowInQueue = false;
+            }
+
+            if (q.first != 0) {
+                dq.push_front(a);
+                if (toppestLow == a) {
+                    toppestLowInQueue = true;
+                }
+            }
+
+            assert(check(a, b, payed, gas,toppestLow, dq));
+        }
+
+        while(b<bb) {
+            //cerr << "b >>\n";
+            b++;
+
+            gas += G[b];
+
+            if (P[b] < P[toppestLow]) {
+                //cerr << "wheert?\n";
+                toppestLow = b;
+                toppestLowInQueue = false;
+            }
+
+            //cerr << toppestLow << " " << P[toppestLow] << "toppestLow and price\n";
+            if (gas < W[b]) {
+                ll needGas = W[b]-gas;
+                gas += needGas;
+                payed += P[toppestLow]*needGas;
+                if ( !toppestLowInQueue) {
+                    dq.push_back(toppestLow);
+                    toppestLowInQueue = true;
+                }
+            }
+
+            gas -= W[b];
+            assert(check(a,b, payed, gas, toppestLow, dq));
+        }
+
+        
+        while(b>bb) {
+            //cerr << "b <<\n";
+
+            if ( dq.empty() ) {
+                //cerr << "empty\n";
+                payed = 0;
+                pll q = seg.query(a, b-1);
+                assert(q.first == 0);
+                gas = q.second;
+                if ( toppestLow == b ) {
+                    toppestLow = a;
+                    toppestLowInQueue = false;
+                }
+            }
+            else {
+
+                int i = dq.back();
+
+                pll q = seg.query(i, b);
+
+                if ( b != i )  {
+                    //cerr << "i " << i << '\n';
+
+                    pll qq = seg.query(i, b-1);
+
+                    ll prevGas;
+                    if (dq.size() > 1) {
+                        dq.pop_back();
+                        int j = dq.back();
+                        dq.push_back(i);
+                        pll pq = seg.query(j, i-1);
+                        prevGas = pq.second;
+                    }
+                    else {
+                        pll pq = seg.query(a, i-1);
+                        prevGas = pq.second;
+                    }
+
+                    payed -= P[i] * (q.first - prevGas);
+
+                    //cerr << "q " << q.first << " " << q.second << endl;
+                    //cerr << "qq " << qq.first << " " << qq.second << endl;
+                    //cerr << "prevGas " << prevGas << endl;
+
+                    if ( qq.first > prevGas) {
+                        //cerr << "if?????????????\n";
+                        payed += P[i] * (qq.first - prevGas);
+                        gas = qq.second;
+                        if ( toppestLow == b ) {
+                            toppestLow = i;
+                            toppestLowInQueue = true;
+                        }
+                    }
+                    else {
+                        //cerr << "else\n";
+                        dq.pop_back();
+                        toppestLow = i;
+                        toppestLowInQueue = false;
+                        gas = qq.second + (prevGas - qq.first);
+                    }
+                }
+                else {
+                    ll prevGas;
+                    if (dq.size() > 1) {
+                        dq.pop_back();
+                        int j = dq.back();
+                        dq.push_back(i);
+                        pll pq = seg.query(j, i-1);
+                        prevGas = pq.second;
+                    }
+                    else {
+                        pll pq = seg.query(a, i-1);
+                        prevGas = pq.second;
+                    }
+
+                    payed -= P[i] * (q.first - prevGas);
+
+                    gas = prevGas;
+
+                    dq.pop_back();
+                    if(!dq.empty()) {
+                        toppestLow = dq.back();
+                        toppestLowInQueue = true;
+                    }
+                    else {
+                        toppestLow = a; // 
+                        toppestLowInQueue = false;
+                    }
+
+                }
+            }
+            b--;
+            assert(check(a,b, payed, gas,toppestLow, dq));
+        }
+        //cerr << "And the anser for " << aa << " " << bb << " is : " << payed << "\n";
+
+        ans[reord[ii]] = payed;
+    }
+
+    for (int i=0; i< q; i++) {
+        cout << ans[i] << endl;
+    }
+}
+
+
