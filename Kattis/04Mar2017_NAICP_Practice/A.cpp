05df9d23b9c871643ce290fadc622af2f8d863e8
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2018-06-27 18:59:23 -0400

    Add ./Kattis/04Mar2017_NAICP_Practice/A.cpp

---
+++ Kattis/04Mar2017_NAICP_Practice/A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n; 
+    cin << n;
+
+    vector<char> rank(n);
+    vector<char> suit(n);
+
+    for (int i=0; i<n; i++) {
+        string s;
+        cin >> s;
+        if 
+        
+
+    int min = 10000000;
+
+    for(int a0=0; a0<4; a0++) {
+    for(int a1=0; a1<4; a1++) {
+    for(int a2=0; a2<4; a2++) {
+    for(int a3=0; a3<4; a3++) {
+        if (a0!=a1 && a0!=a2 && a0!=a3 
+                && a1!=a2 && a1!=a3
+                && a2!=a3) {
+
+            int ord[4] = {a0, a1, a2, a3};
+
+            int count = 0;
+            for (int asc=0; asc<=15; asc++) {
+                for (int i=0; i<
+
+
+
+    
+
+}
