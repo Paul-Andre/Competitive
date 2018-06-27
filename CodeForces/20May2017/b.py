77a464e5c1454f84bdd9945549f361175750f793
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2018-06-27 18:59:22 -0400

    Add ./CodeForces/20May2017/b.py

---
+++ CodeForces/20May2017/b.py
@@ -0,0 +1,19 @@
+n,f = map(int, input().split())
+a = []
+for _ in range(n):
+    k,l = map(int, input().split())
+    a.append( ( -(min(l, k*2)-min(l,k)) , min(l, k)) )
+
+a.sort()
+
+#print(a)
+
+tot = 0;
+for i in range(0,f):
+    tot += -a[i][0] + a[i][1]
+
+for i in range(f,n):
+    tot += a[i][1]
+
+print(tot)
+
