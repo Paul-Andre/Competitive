ceed27205ae75271cb06c5737a68e581def6a87f
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2019-02-03 19:35:16 -0500

    Add ./CodeForces/1107/b.cpp

---
+++ CodeForces/1107/b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve() {
+  ll k,x;
+  cin >> k >> x;
+  k--;
+  cout << k*9 + x << endl;
+}
+
+int main() {
+  int NNN;
+  cin >> NNN;
+  while(NNN--){
+    solve();
+  }
+} 
