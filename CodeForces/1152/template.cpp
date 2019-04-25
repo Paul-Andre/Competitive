7a9b1712a360d6da10056a264594aa9c91a5cef1
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2019-04-24 21:57:04 -0400

    Add ./CodeForces/1152/template.cpp

---
+++ CodeForces/1152/template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#include <cmath> 
+
+using namespace std;
+typedef long long ll;
+
+#ifndef ONLINE_JUDGE
+#  define LOG(x) (cerr << #x << " = " << (x) << endl)
+#else
+#  define LOG(x) 0
+#endif
+
+
+int main() {
+}
