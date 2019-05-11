1ba26ecafa83f712b45b2dccd209f367e802b0e8
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2019-05-11 11:41:50 -0400

    Add ./CodeForces/1156/c.cpp

---
+++ CodeForces/1156/c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#include <cmath> 
+
+using namespace std;
+typedef long long ll;
+
+#ifndef ONLINE_JUDGE
+#  define LOG(x) (cerr << #x << " = " << (x) << endl)
+#else
+#  define LOG(x) 0
+#endif
+
+
+int n, z;
+int X[212345];
+int main() {
+  cin >>n>>z;
+  for (int i=0; i<n; i++) {
+    cin >> X[i];
+  }
+}
