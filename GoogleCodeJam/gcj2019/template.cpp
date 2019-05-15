7de84fc9bf5d5336a9ecb8465d9e0a96ee515c38
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2019-05-15 11:02:29 -0400

    Add ./GoogleCodeJam/gcj2019/template.cpp

---
+++ GoogleCodeJam/gcj2019/template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#include <cmath> 
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#ifndef ONLINE_JUDGE
+#  define LOG(x) (cerr << #x << " = " << (x) << endl)
+#else
+#  define LOG(x) 0
+#endif
+
+
+void solve() {
+
+}
+
+int main() {
+  int T;
+  cin >> T;
+  for (int ttt=1; ttt<=T; ttt++) {
+    cout << "Case #" << ttt << ": ";
+    solve();
+    cout << endl;
+  }
+}
