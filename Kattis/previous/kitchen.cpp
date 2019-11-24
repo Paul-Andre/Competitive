97f710888455f9642a3040895d063f82723dced0
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2019-11-24 05:20:17 -0500

    Add ./Kattis/previous/kitchen.cpp

---
+++ Kattis/previous/kitchen.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++>
+using namespace std;
+
+vector<int> C;
+int main() {
+  int n;
+  cin >> n;
+  for (int i=0; i<n; i++) {
+    int a;
+    cin >> a;
+    C.push_back(a);
+  }
+  int V;
+  cin >> V;
+
+
