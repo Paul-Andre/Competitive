8a5ac00f1c9e8ebb4de0627109e8b99bf1302a9e
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2020-08-10 10:50:16 -0400

    Add ./CodeForces/1393/a.cpp

---
+++ CodeForces/1393/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  // ios_base::sync_with_stdio(false); cin.tie(NULL);
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    int a=(n+2)/2;
+    cout<<a<<endl;
+  }
+}
+
