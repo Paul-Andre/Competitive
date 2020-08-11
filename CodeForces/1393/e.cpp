abb59d599656e19063e7ec3af3ebd02506752ea0
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2020-08-10 23:15:15 -0400

    Add ./CodeForces/1393/e.cpp

---
+++ CodeForces/1393/e.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  // ios_base::sync_with_stdio(false); cin.tie(NULL);
+  int n;
+  vector<string> A(n);
+  for(int i=0;i<n;i++){
+    cin>>A[i];
+  }
+  vector<
+
+}
