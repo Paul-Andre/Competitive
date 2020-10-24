f84fcb008c576a68b31f4d2de5e43ce48fea10ed
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2020-10-24 19:32:24 -0400

    Add ./IEEExtreme/IEEExtreme2020/magical-stones-2-old.cpp

---
+++ IEEExtreme/IEEExtreme2020/magical-stones-2-old.cpp
@@ -0,0 +1,206 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+
+int B[121];
+int W[121];
+
+bool vis[121][121];
+char trans[121][121];
+int steps[121][121];
+
+int n;
+void printThing() {
+
+    for (int i=1; i<=n;i++){
+      cerr << i << ": ";
+      for (int j=1; j<=n; j++) {
+        if (trans[i][j]) {
+          cerr <<trans[i][j] << " ";
+        } else {
+          cerr << "~" << " " ;
+        }
+      }
+      cerr << endl;
+    }
+}
+  
+
+vector<pair<int,int>> toRecheck;
+
+void doThings(int i, int j) {
+
+  if (vis[i][j]) return;
+  vis[i][j] = true;
+
+  cerr << "i j " << i << " " << j <<endl;
+  printThing();
+
+  trans[i][j] = '?';
+
+
+  shared_ptr<void> _(nullptr, bind([&]{
+
+        cerr << "i j " << i << " " << j <<endl;
+
+        printThing();
+        cerr << endl;
+
+        }));
+
+  if (i == j) {
+    trans[i][j] = '.';
+    return;
+  }
+
+  bool needRecheck = false;
+  {
+    int ii = W[i];
+    int jj = W[j];
+    doThings(ii, jj);
+    if (trans[ii][jj] == '?') {
+      needRecheck = true;
+    } else if (trans[ii][jj] != 'x') {
+      trans[i][j] = 'W';
+      return;
+    } 
+  }
+  {
+    int ii = B[i];
+    int jj = B[j];
+    doThings(ii, jj);
+    if (trans[ii][jj] == '?') {
+      needRecheck = true;
+    } else if (trans[ii][jj] != 'x' ) {
+      trans[i][j] = 'B';
+      return;
+    } 
+  }
+  if (!needRecheck) {
+    trans[i][j] = 'x';
+  } else {
+    toRecheck.push_back(pair<int,int> (i, j));
+
+  }
+}
+
+bool A[121];
+bool tmp[121];
+
+void cast(char c){
+  if (c=='W') {
+    for (int i=1; i<=n; i++) {
+      if (A[i]) {
+        tmp[W[i]] = true;
+      }
+    }
+  }else if (c=='B') {
+    for (int i=1; i<=n; i++) {
+      if (A[i]) {
+        tmp[B[i]] = true;
+      }
+    }
+  }
+  for (int i=1; i<=n; i++) {
+    A[i]=tmp[i];
+  }
+}
+
+pair<int,int> findPair() {
+  
+  bool lookingFirst = true;
+  int x = 0;
+  int y = 0;
+  for (int i=1; i<=n; i++) {
+    if (A[i]) {
+      if (lookingFirst) {
+        x=i;
+        y=i;
+        lookingFirst = false;
+      } else {
+        y=i;
+        break;
+      }
+    }
+  }
+  return pair<int,int>(x,y);
+}
+
+int main() {
+  int t;
+  cin>>t;
+  while(t--) {
+    cin>>n;
+    for (int i=1; i<= n;i++) {
+      int x;
+      cin>>x;
+      W[i] = x;
+    }
+    for (int i=1; i<= n;i++) {
+      int x;
+      cin>>x;
+      B[i] = x;
+    }
+
+    for (int i=1; i<=n;i++){
+      for (int j=1; j<=n; j++) {
+        if (!vis[i][j]) {
+          doThings(i,j);
+        }
+      }
+    }
+    for (auto p: needRecheck) {
+      doThings(p.first
+    exit(0);
+
+
+    bool good = true;
+    for (int i=1; i<=n; i++){
+      for (int j=1; j<=n; j++) {
+        if (!trans[i][j]) {
+          good = false;
+        }
+      }
+    }
+    if (!good) {
+      cout << "impossible\n";
+      continue;
+    }
+
+    for (int i=0; i<121;i++){
+      A[i] = 0;
+    }
+    for (int i=1; i<=n;i++){
+      A[i] = 1;
+    }
+    
+    string out;
+    while(true) {
+      pair<int,int> p = findPair();
+      if (p.first == p.second) {
+        break;
+      }
+      int i = p.first;
+      int j = p.second;
+      while(i!=j) {
+        char instr = trans[i][j];
+        cerr << i<<" " << j <<endl;
+        out.push_back(instr);
+        cast(instr);
+        if (instr == 'W') {
+          i = W[i];
+          j = W[j];
+        } else if (instr == 'B') {
+          i = B[i];
+          j = B[j];
+        }
+      }
+    }
+
+    cout << out <<endl;
+  }
+}
+
+
