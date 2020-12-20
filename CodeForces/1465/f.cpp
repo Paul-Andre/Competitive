04093facb121a54c80b0d2c8d732f50be9beab21
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2020-12-20 15:25:49 -0500

    Add ./CodeForces/1465/f.cpp

---
+++ CodeForces/1465/f.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    vector<int> P(n+1,0);
+    for(int i=1; i<=n; i++) {
+      int p;
+      cin >> p;
+      P[i]=p;
+    }
+  }
+}
