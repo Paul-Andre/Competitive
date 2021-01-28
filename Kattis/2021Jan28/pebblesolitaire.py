a643c81af485ae488f5d410528b9e3b4f7057e5c
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2021-01-28 13:14:25 -0500

    Add ./Kattis/2021Jan28/pebblesolitaire.py

---
+++ Kattis/2021Jan28/pebblesolitaire.py
@@ -0,0 +1,32 @@
+n = int(input())
+sol = {}
+def get(s):
+    if (s in sol):
+        return sol[s]
+    ss = s
+    s = list(s)
+    m = s.count('o')
+    for i in range(0,12-2):
+        if (s[i]=='o' and
+                s[i+1]=='o' and
+                s[i+2]=='-' ):
+            ns = list(s)
+            ns[i]='-'
+            ns[i+1]='-'
+            ns[i+2]='o'
+            m = min(m,get("".join(ns)))
+        if (s[i]=='-' and
+                s[i+1]=='o' and
+                s[i+2]=='o' ):
+            ns = list(s)
+            ns[i]='o'
+            ns[i+1]='-'
+            ns[i+2]='-'
+            m = min(m,get("".join(ns)))
+    sol[ss] = m
+    return m
+
+for _ in range(n):
+    s = input()
+    print(get(s))
+
