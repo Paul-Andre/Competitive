16a954eaf43885816fb258087e1832834a1add11
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2021-05-15 10:00:13 -0400

    Add ./GoogleCodeJam/gcj2021/2/template.cpp

---
+++ GoogleCodeJam/gcj2021/2/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+}
+
+
+int main(){
+  int T;
+  cin>>T;
+  for(int ttt=1; ttt<=T; ttt++){
+    cout << "Case #"<<ttt<<": ";
+    solve();
+    cout <<endl;
+  }
+}
