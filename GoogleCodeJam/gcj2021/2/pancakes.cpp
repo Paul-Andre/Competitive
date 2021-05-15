fee2ee15e80f752d31c7ccd4e06d7ab5abdeab28
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2021-05-15 10:43:29 -0400

    Add ./GoogleCodeJam/gcj2021/2/pancakes.cpp

---
+++ GoogleCodeJam/gcj2021/2/pancakes.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define MOD 1000000007
+
+void solve(){
+  int n;
+  cin>>n;
+  vector<ll> V(n);
+  for(auto&a:V)cin>>a;
+
+}
+
+
+int main(){
+  int T;
+  cin>>T;
+  for(int ttt=1; ttt<=T; ttt++){
+    cout << "Case #"<<ttt<<": ";
+    solve();
+    cout <<endl;
+  }
+}
