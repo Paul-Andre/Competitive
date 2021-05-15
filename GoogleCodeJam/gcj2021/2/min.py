fc3f309e374031f9f4ef9018d30b943b064ea629
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2021-05-15 10:12:18 -0400

    Add ./GoogleCodeJam/gcj2021/2/min.py

---
+++ GoogleCodeJam/gcj2021/2/min.py
@@ -0,0 +1,12 @@
+t,n=map(int,input().split())
+for tt in range(t):
+    for i in range(1,100):
+        print("M",i,100)
+        p = int(input())
+        if(i!=p):
+            print("S",i,p)
+            _ =input()
+    print("D")
+    o =input()
+    if(o==-1): break
+
