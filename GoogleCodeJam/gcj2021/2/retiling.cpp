310f055e86bfb87e51c07c9b982c89a78d3e4c68
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2021-05-15 20:30:04 -0400

    Add ./GoogleCodeJam/gcj2021/2/retiling.cpp

---
+++ GoogleCodeJam/gcj2021/2/retiling.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+  int r,c,f,s;
+  cin>>r>>c>>f>>s;
+  vector<string> A;
+  for(auto&a:A)cin>>a;
+  vector<string> B;
+  for(auto&a:B)cin>>a;
+
+}
+
+
+int main(){
+  int T;
+  cin>>T;
+  for(int ttt=1; ttt<=T; ttt++){
+    cout << "Case #"<<ttt<<": ";
+    solve();
+    cout <<endl;
+  }
+}
