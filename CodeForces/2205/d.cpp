5daa75d792dd21babfbb36ae0edcb17f5ade2ac3
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2026-03-19 11:58:24 -0400

    2205 post

--- CodeForces/2205/d.cpp
+++ CodeForces/2205/d.cpp
@@ -3,8 +3,79 @@ using namespace std;
 
 typedef long long ll;
 
+#define cerr if(false) cerr
 
+int join_function(int x, int y) {
+  return max(x, y);
+}
 
+int default_function() {
+  return 0;
+}
+
+enum NodeType {
+  EMPTY,
+  SINGLE,
+  MULTIPLE,
+};
+
+struct Tree {
+  int lower, upper;
+  Tree *l, *r;
+
+  NodeType type;
+  int position;
+  int value;
+
+  Tree(int _lower, int _upper) {
+    lower = _lower;
+    upper = _upper;
+    l = r = NULL;
+    type = EMPTY;
+    assert(lower <= upper);
+  }
+  void set(int pos, int val) {
+    if (pos < lower || pos > upper) return;
+    if (type == EMPTY) {
+      position = pos;
+      value = val;
+      type = SINGLE;
+    } else {
+      if (type == SINGLE) {
+        int mid = (lower + upper)/ 2;
+        l = new Tree(lower, mid);
+        r = new Tree(mid+1, upper);
+        l->set(position, value);
+        r->set(position, value);
+        value = join_function(l->getAll(), r->getAll());
+        type = MULTIPLE;
+      }
+      assert(type == MULTIPLE);
+      l->set(pos, val);
+      r->set(pos, val);
+      value = join_function(l->getAll(), r->getAll());
+    }
+  }
+  int getAll() {
+    if (type == EMPTY) {
+      return default_function();
+    }
+    return value;
+  }
+  int get(int low, int up) {
+    if(low > up)return default_function();
+    if (low > upper || up < lower) return default_function();
+    if (type == EMPTY) {
+      return default_function();
+    } else if (type == SINGLE) {
+      if (low <= position && position <= up) return value;
+      else return default_function();
+    } else {
+      assert(type == MULTIPLE);
+      return join_function(l->get(low, up), r->get(low, up));
+    }
+  }
+};
 
 int main() {
   int t;
@@ -34,19 +105,25 @@ int main() {
     {
       int running_max = -1;
 
-      // D[i] = the length of the longest valid decreasing sequence that ends at value i
-      map<int,int> D;
+      // tree[l, r] = longest decreasing sequence so far that ends between l and r
+      Tree tree(1, n);
 
       for (int i=0; i<A.size(); i++) {
 
 
         int a = A[i];
+        bool new_max = (a > running_max) ;
+
+        if (new_max) {
+          running_max = a;
+          tree = Tree(1,n);
+        }
         //cerr<<(a+1)<<" "<<n <<endl;
         int tv = tree.get(a+1, n);
         int val = tv + 1;
         tree.set(a, val);
         B[i] = val;
-        if (i>0) {
+        if (i>0 && !new_max) {
           B[i] = max(B[i], B[i-1]);
         }
         cerr << B[i] << " ";
@@ -54,18 +131,26 @@ int main() {
       cerr<<endl;
     }
     {
+      int running_max = -1;
       // tree[l, r] = longest decreasing sequence so far that ends between l and r
       Tree tree(1, n);
 
       for (int i=A.size()-1; i>=0; i--) {
         int a = A[i];
+        bool new_max = (a > running_max) ;
+        if (new_max) {
+          running_max = a;
+          tree = Tree(1,n);
+        }
         int tv = tree.get(a+1, n);
         int val = tv + 1;
         tree.set(a, val);
         C[i] = val;
-        if (i<C.size()-1) {
+        if (i<C.size()-1 && !new_max) {
           C[i] = max(C[i], C[i+1]);
         }
+      }
+      for (int i=0; i<C.size(); i++) {
         cerr << C[i] << " ";
       }
       cerr<<endl;
@@ -75,7 +160,6 @@ int main() {
     for (int i=0; i<A.size()-1; i++) {
       longest = max(longest, B[i]+C[i+1]);
     }
-    return longest;
-}
+    cout<<(n-longest) <<endl;
   }
 }
